f3fe1456f89106da0613527033d74ef1782b1fd0
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-03-12 18:57:41 +0900

    feat: 앱 구조 및 탭 네비게이션 설정, 기본 화면 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
     return (
         <View className="flex-1 justify-center items-center">
             <Text className="text-5xl font-bold text-dark-300">Welcome</Text>
+            <Link href="/movie/123">
+                <Text>Go to movie details</Text>
+            </Link>
         </View>
     );
 }
