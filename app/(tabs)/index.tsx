7bea3fae2e515b35db6b04d15d643baa63cb22fc
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-03-14 13:01:24 +0900

    feat: 최신 영화 목록 표시를 위한 MovieCard 컴포넌트 추가 및 메인 화면에 영화 데이터 로딩 기능 구현

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,28 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { Link, useRouter } from "expo-router";
-import { Image, ScrollView, Text, View } from "react-native";
-
+import {
+    ActivityIndicator,
+    FlatList,
+    Image,
+    ScrollView,
+    Text,
+    View
+} from "react-native";
+import React from "react";
+import MovieCard from "@/components/MovieCard";
 export default function Index() {
     const router = useRouter();
+
+    const {
+        data: movies,
+        error: moviesError,
+        loading: moviesLoading,
+        refetch: refetchMovies
+    } = useFetch(() => fetchMovies({ query: "" }));
 
     return (
         <View className="flex-1 bg-primary">
@@ -22,12 +39,45 @@ export default function Index() {
                     source={icons.logo}
                     className="w-12 h-10 mt-20 mb-5 mx-auto"
                 />
-                <View className="flex-1 mt-5 ">
-                    <SearchBar
-                        onPress={() => router.push("/search")}
-                        placeholder="Search for a movie"
+                {moviesLoading ? (
+                    <ActivityIndicator
+                        size="large"
+                        color="#fff"
+                        className="mt-10 self-center"
                     />
-                </View>
+                ) : moviesError ? (
+                    <Text className="text-white text-center text-lg">
+                        {moviesError.message}
+                    </Text>
+                ) : (
+                    <View className="flex-1 mt-5">
+                        <SearchBar
+                            onPress={() => router.push("/search")}
+                            placeholder="Search for a movie"
+                        />
+                        <>
+                            <Text className="text-white text-center text-lg">
+                                Latest movies
+                            </Text>
+                            <FlatList
+                                data={movies?.results}
+                                renderItem={({ item }) => (
+                                    <MovieCard {...item} />
+                                )}
+                                keyExtractor={(item) => item.id.toString()}
+                                numColumns={3}
+                                columnWrapperStyle={{
+                                    justifyContent: "flex-start",
+                                    marginBottom: 10,
+                                    gap: 20,
+                                    paddingRight: 5
+                                }}
+                                className="mt-2 pb-32"
+                                scrollEnabled={false}
+                            />
+                        </>
+                    </View>
+                )}
             </ScrollView>
         </View>
     );
